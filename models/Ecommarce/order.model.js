8723544fdf6305a966b05f8721ee97735aadfd51
sangamyadav-dev <sangamyadav95k@gmail.com>
2024-09-11 09:47:16 +0530

    E COMMARCE DATA MODELS

--- models/Ecommarce/order.model.js
+++ models/Ecommarce/order.model.js
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 
+const orderItemSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'product'
+    },
+    quantity:{
+        type:Number,
+        required: true
+    }
+})
+
 const orderSchema = new mongoose.Schema({
     orderPrice:{
         type: String,
@@ -8,6 +19,19 @@ const orderSchema = new mongoose.Schema({
     customer:{
         type: mongoose.Schema.Types.ObjectId,
         ref:'User'
+    },
+    orderItems:{
+        type:[orderItemSchema]
+    },
+    address:{
+        type:String,
+        required: true
+    },
+    status:{
+        type:String,
+        enum:['ORDER PENDING', 'ORDER PLACED', 'ORDER CANCELLED', 'ORDER DELIVERED'],
+        default:'ORDER PENDING'
+
     }
 },{timestamps:true});
 
